e8ac5492b0866940d135b67d9e03c18af65c2459
juanmanuel0963 <juanmanuel0963@gmail.com>
2024-05-20 16:07:59 -0500

    commit

--- microservices_reminderx/models/commitments_model.go
+++ microservices_reminderx/models/commitments_model.go
@@ -14,13 +14,19 @@ type Commitment struct {
 }
 
 type Commitment_Get struct {
-	gorm.Model                     // Includes fields: ID, CreatedAt, UpdatedAt, DeletedAt
-	Commitment      string         `json:"commitment"`
-	Date            datatypes.Date `json:"date"`
-	ClientID        int
-	Client          Client `gorm:"foreignKey:ClientID"` // use AdminID as foreign key
-	ClientFirstName string `json:"clientFirstName"`
-	ClientSurName   string `json:"clientSurName"`
-	AdminFirstName  string `json:"adminFirstName"`
-	AdminSurName    string `json:"adminSurName"`
+	gorm.Model                       // Includes fields: ID, CreatedAt, UpdatedAt, DeletedAt
+	Commitment        string         `json:"commitment"`
+	Date              datatypes.Date `json:"date"`
+	ClientID          int
+	Client            Client `gorm:"foreignKey:ClientID"` // use AdminID as foreign key
+	ClientFirstName   string `json:"clientFirstName"`
+	ClientSurName     string `json:"clientSurName"`
+	ClientCountryCode string `json:"clientCountryCode"`
+	ClientPhoneNumber string `json:"clientPhoneNumber"`
+	ClientEmail       string `json:"clientEmail"`
+	AdminFirstName    string `json:"adminFirstName"`
+	AdminSurName      string `json:"adminSurName"`
+	AdminCountryCode  string `json:"adminCountryCode"`
+	AdminPhoneNumber  string `json:"adminPhoneNumber"`
+	AdminEmail        string `json:"adminEmail"`
 }
